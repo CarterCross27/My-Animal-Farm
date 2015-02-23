ac706ee81bfef231232ae2d0e595d9f213ee1a0d
CarterCross27 <ccrossett@macmini-lab14.local>
2015-02-23 10:20:49 -0500

    Copied over turtle elements to dolphin file

---
+++ CartersDolphin.java
@@ -0,0 +1,34 @@
+public class CartersDolphin extends Dolphin
+{ 
+  /**
+   * Constructor that takes a model display and adds
+   * a turtle in the middle of it
+   * @param display the model display
+   */
+  public CartersDolphin(World wref)
+  {
+    super(wref);
+    Color shellColor = new Color(200, 50, 10); 
+    this.setShellColor(shellColor);
+    Color bodyColor = new Color(250, 30, 10);
+    this.setBodyColor(bodyColor);
+    Color infoColor = new Color(10, 10, 10);
+    this.setInfoColor(infoColor);
+    Color infoColor = new Color(10, 10, 10);
+    this.setWidth(10);
+    this.setHeight(25):
+    
+  }
+  
+  /**
+   * Constructor that takes the x and y position and the
+   * model displayer
+   * @param x the x pos
+   * @param y the y pos
+   * @param display the model display
+   */
+  public CartersDolphin(int x, int y, World wref)
+  {
+    super(x, y, wref);
+  }
+}
