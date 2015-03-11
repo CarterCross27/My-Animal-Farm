ac7378d5a27e6613701828ca948223257bb7166f
Jdariano <jdar15@lcscourses.net>
2015-03-11 09:55:31 -0400

    Russell made his moves on the board. he's the real deal

--- Farm.java
+++ Farm.java
@@ -5,5 +5,33 @@ public class Farm
        World wref = new World();
        CartersDolphin ex1 = new CartersDolphin(wref);
        Squirrel russell=new Squirrel(wref);
+       russell.setWidth(10);
+    russell.setHeight(25);
+    russell.forward(100);
+    russell.turnRight();
+    russell.forward(50);
+    russell.turnRight();
+    russell.forward(100);
+    russell.turnLeft();
+    russell.forward(70);
+    russell.turnLeft();
+    russell.forward(90);
+    russell.turnRight();
+    russell.forward(20);
+    russell.turnRight();
+    russell.forward(100);
+    russell.turnRight();
+    russell.forward(20);
+    russell.turnRight();
+    russell.forward(90);
+    russell.turnLeft();
+    russell.forward(80);
+    russell.turnLeft();
+    russell.forward(100);
+    russell.turnRight();
+    russell.forward(20);
+    russell.turnRight();
     }
+
+    
 }
